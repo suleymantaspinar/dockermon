ef2be2372e3ef3654dbf441b861c7b66288eb90a
Suleyman Taspinar <suleyman@pranageo.com>
2021-08-20 01:26:25 +0300

    Move dockerfile and sh files to home dir

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,12 @@
 #!/usr/bin/env node
-
-require('../lib/generate-docker');
 const { Command } = require('commander');
 const program = new Command();
-var cp = require('child_process');
+const { buildThings } = require('../lib/prepare')
 
 program.version('0.0.1');
 
 program
-  .description('This is the very first version of dockermon')
+  .description('This is the very first version of dockermon. Start testing')
   .command('start [path]')
   .option('--test', 'Log the test option')
-  .action(
-    // buildDockerImage().createDockerVolume().runDockerContainer()
-    cp.exec('chmod +x lib/build-docker.sh & lib/build-docker.sh ', function(err, stdout, stderr) {
-      if(err) console.log("1", err)
-      if(stderr) console.log("2", stderr)
-    })
-  )
+  .action(buildThings())
